806e5697bbd60b8882f2e634877621517f271a9f
George Travis <wewerelikesharks@gmail.com>
2020-01-25 18:08:56 -0800

    main response json

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const axios = require('axios');
 const util = require('util');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const writeFile = fs.writeFile;
 
 // const readFileAsync = util.promisify(readFile);
-// const writeFileAsync = util.promisify(writeFile);
+const writeFileAsync = util.promisify(writeFile);
 
 async function getGitJson() {
   try {
@@ -18,7 +20,8 @@ async function getGitJson() {
     const response = await axios.get(
       `https://api.github.com/users/${username}`
     );
-    console.log(response.data);
+    // const content = JSON.stringify(response.data, null, 2);
+    // await writeFileAsync('mainresponse.json', content, 'utf8');
     // console.log(response.data);
   } catch (err) {
     console.error(err);
