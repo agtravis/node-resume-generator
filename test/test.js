fb0cfc8cb1dfc2e8c9e6c57b0e9227cd3765459b
George Travis <wewerelikesharks@gmail.com>
2020-02-01 09:11:33 -0800

    added more unit tests

--- test/test.js
+++ test/test.js
@@ -39,16 +39,28 @@ describe('css color scheme', function() {
 });
 
 describe('async functions', () => {
+  const username = 'agtravis';
+
   it('should match name', async () => {
-    const username = 'agtravis';
     const expected = 'Alexander George Travis';
     const actual = await html.getUserJSON(username);
     assert.equal(actual.data.name, expected);
   });
 
-  it('should be an array', async () => {
-    const username = 'agtravis';
+  it('the repos return should be an array', async () => {
     const actual = await html.getUserJSONRepos(username);
     chai.expect(actual.data).to.be.an('array');
+  });
+
+  it('get stars should return a number', async () => {
+    const actual = await html.getStars(username);
+    chai.expect(actual).to.be.a('number');
+  });
+
+  it('array of repos should have this number of repos in it', async () => {
+    const repos = await html.getUserJSONRepos(username);
+    const repoArrStr = html.fillRepoArray(repos);
+    const repoArr = JSON.parse(repoArrStr);
+    assert.equal(repos.data.length, repoArr.length);
   });
 });
